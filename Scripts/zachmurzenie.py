517d8d9067432507346b39b2a36380db5cbb9b4c
gallanonimm <kacperek.bilko@gmail.com>
2015-06-05 00:08:21 +0200

    upgrade of cloud coverage tool - added standard deviation

--- Scripts/zachmurzenie.py
+++ Scripts/zachmurzenie.py
@@ -1,4 +1,13 @@
 from __future__ import division
+import numpy as np
+
+
+def odchylenie(x):
+    wariancja = 0.0
+    average = sum(x) / len(x)
+    for i in x:
+        wariancja += (i - average) ** 2
+    return np.sqrt(wariancja / len(x)) / 100
 
 with open('dane_zachmurzenie.txt', 'r') as plik:    x = [float(liczba) for liczba in plik.readlines()[0].split(' ')]
 pk = open('zachmurzenie.csv', 'w')
@@ -8,7 +17,8 @@ ilosc_paseczkow = int(krok / 2.5)
 
 for i in range(0, len(x), 144 * ilosc_paseczkow):
     pk.write(str(licznik) + ',' + str(
-        sum(x[i:i + 144 * ilosc_paseczkow]) / len(x[i:i + 144 * ilosc_paseczkow]) / 100) + '\n')
+        sum(x[i:i + 144 * ilosc_paseczkow]) / len(x[i:i + 144 * ilosc_paseczkow]) / 100) + ',' + str(
+        odchylenie(x[i:i + 144 * ilosc_paseczkow])) + '\n')
     licznik += krok
 pk.close()
-# wyrzucony plik w formacie latitude,average cloud coverage [%]
+# wyrzucony plik w formacie latitude,average cloud coverage,standard deviation
