eb974b99f6aceb096ce0944af94c514d1855b2dc
Karol Kozicki <kozicki.karol0@gmail.com>
2015-06-04 19:23:12 +0200

    Add creating zones

--- data/Earth.py
+++ data/Earth.py
@@ -1,14 +1,27 @@
 import datetime
+from data.Zone import Zone
+
 
 class Earth():
 
     DATE = datetime.datetime(year=2015, month=1, day=15)
 
-    def __init__(self):
+    def __init__(self, division):
         self.radius = 6371
+        self.division = division
+        self.zones = self.create_zones()
 
     def get_area(self, lat1, lat2):
         pass
 
     def get_month(self):
         return Earth.DATE.month
+
+    def create_zones(self):
+        step = 180/self.division
+        zones = []
+        start = -90
+        while start < 90:
+            zones.append(Zone(start, start+step, self.get_area(start, start+step)))
+            start += step
+        return zones
--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from data.Earth import *
 from data.Radiation import *
 from data.SurfaceType import *
 from data.Zone import *
+from dateutil.relativedelta import relativedelta
 
-earth = Earth()
+earth = Earth(6)
+until = datetime.datetime(year=2100, month=1, day=15)
 
+
+while earth.DATE < until:
+    #DO STH
+    earth.DATE += relativedelta(months=1)
 #test
-print(earth.radius)
+print(earth.zones)
 
 # calculate absorbed energy (radiation) for every zone -> temperature
 # zone temperature -> calculate emitted energy (radiation) for every zone
